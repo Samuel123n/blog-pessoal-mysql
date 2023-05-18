31d1c199cd05294dd10bf4797549b435054080d5
Samuel123n <souzasamuka2017@gmail.com>
2023-05-18 11:47:13 -0300

    Atualização controller

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -3,10 +3,14 @@ package com.generation.blogpessoal.model;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -27,6 +31,10 @@ public class Postagem {
 	@Size(min = 10, max = 100, message = "Este atributo tem que ter no mínimo 10 caracteres e no máximo 100 caracteres")
 	private String texto;
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
+	
 	@UpdateTimestamp
 	private LocalDateTime data;
 
@@ -61,5 +69,14 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
 
 }
